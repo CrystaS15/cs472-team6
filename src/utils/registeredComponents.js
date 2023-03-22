b7369b05e09cc959b5f196ba12a34b9bf8603cc6
Kristy Nguyen <kln95624@gmail.com>
2023-03-21 17:35:01 -0700

    Deleted files from AboutHeadSection component to separate component issues.

--- src/utils/registeredComponents.js
+++ src/utils/registeredComponents.js
@@ -2,10 +2,6 @@ import Banner from "../components/Banner/Banner";
 import BannerOptions from "../components/Banner/BannerOptions";
 import BannerPreview from "../components/Banner/BannerPreview.png";
 
-import AboutHeadSection from "../components/AboutHeadSection/AboutHeadSection";
-import AboutHeadSectionOptions from "../components/AboutHeadSection/AboutHeadSectionOptions";
-import AboutHeadSectionPreview from "../components/AboutHeadSection/AboutHeadSectionPreview.png";
-
 import AboutHeadImages from "../components/AboutHeadImages/AboutHeadImages";
 import AboutHeadImagesOptions from "../components/AboutHeadImages/AboutHeadImagesOptions";
 import AboutHeadImagesPreview from "../components/AboutHeadImages/AboutHeadImagesPreview.png";
@@ -19,13 +15,6 @@ import { Gradients } from "./utils";
 
 export const registeredComponents = new Map([
     ['Banner', new Component('Banner', Banner, BannerOptions, BannerPreview, {words: 'Design,Develop,Discover', gradient: Gradients.default})],
-    ['AboutHeadSection', new Component('AboutHeadSection', AboutHeadSection, AboutHeadSectionOptions, AboutHeadSectionPreview, 
-        {   name: 'Kartik Bansal', 
-            gradient: Gradients.default,
-            description: `Over the past 12 years, I\'ve worked with a diverse range of clients, from startups to Fortune 500 companies. 
-                            I love crafting interfaces that delight users and help businesses grow.`
-        })
-    ],
     ['AboutHeadImages', new Component('AboutHeadImages', AboutHeadImages, AboutHeadImagesOptions, AboutHeadImagesPreview, 
         {   image1: {image1}, 
             image2: {image2},
