6ad6439a8f5891a1d18062ac21a07f8d243b6144
Kristy Nguyen <kln95624@gmail.com>
2023-03-21 18:44:12 -0700

    Modified AboutHeadImages component file

--- src/components/AboutHeadImages/AboutHeadImages.jsx
+++ src/components/AboutHeadImages/AboutHeadImages.jsx
@@ -1,36 +1,26 @@
-
-
 export default function AboutHeadImages({options}) {
   const {image1, image2, image3, image4} = options;
 
   return (
-    <div class="container mx-auto px-120 py-482">
-      <div class="flex flex-wrap -mx-4">
+    <div className="flex flex-row justify-between">
+      <div className="w-1/3 mx-4">
         {/*Image 1*/}
-        <div class="w-1/3 px-4">
-          <img src={image1} class="w-full" alt="Image 1" />
-        </div>
+        <img src={image1} alt="Image 1" />
+      </div>
+      
+      <div className="w-1/3 mx-4">
         {/*Image 2*/}
-        <div class="w-1/3 px-4">
-          <img src={image2} class="w-full" alt="Image 2" />
-        </div>
-
-        {/*Images 3 and 4*/}
-        <div class="w-1/3 flex flex-wrap">
+        <img src={image2} alt="Image 2" />
+      </div>
+      
+      <div className="w-1/3 mx-4">    
           {/*Image 3*/} 
-          <div class="w-full md:w-1/2 px-4">
-            <img src={image3} class="w-full h-full object-cover" alt="Image 3" />
-          </div>
+          <img src={image3} alt="Image 3" className="mb-4" />
+          
           {/*Image 4*/}
-          <div class="w-full md:w-1/2 mt-4 md:mt-0 px-4">
-            <img src={image4} class="w-full h-full object-cover" alt="Image 4" />
-          </div>
-
-        </div>
+          <img src={image4} alt="Image 4" />
       </div>
     </div>
-
-
 
   );
 }
